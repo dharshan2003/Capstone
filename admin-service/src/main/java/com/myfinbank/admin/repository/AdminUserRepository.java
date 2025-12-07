ea8fe6961eb5df609284aa15c7ca39d68c280ecd
dharshan2003 <112673415+dharshan2003@users.noreply.github.com>
2025-12-07 12:59:58 +0530

    Delete admin-service directory

--- admin-service/src/main/java/com/myfinbank/admin/repository/AdminUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myfinbank.admin.repository;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.myfinbank.admin.model.AdminUser;
-
-public interface AdminUserRepository extends JpaRepository<AdminUser, Long> {
-    Optional<AdminUser> findByEmail(String email);
-}
